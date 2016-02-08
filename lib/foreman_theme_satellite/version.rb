8249df5458c27b53c287e16cae8b468b860e6f8a
Eric D Helms <ericdhelms@gmail.com>
2016-02-08 12:07:20 -0500

    Update version to X.Y.Z

--- lib/foreman_theme_satellite/version.rb
+++ lib/foreman_theme_satellite/version.rb
@@ -1,5 +1,5 @@
 module ForemanThemeSatellite
-  VERSION = "0.1"
+  VERSION = "0.1.0"
   #this file indicates the satellite version that will be represented on the login page.
   SATELLITE_VERSION = "SATELLITE-6.2.0"
   #this file indicates the satellite version that will be uesd on links to documentation.
