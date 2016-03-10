ddd39d54a20a848707399509c3b675223da2be7d
Mike McCune <mmccune@redhat.com>
2016-03-10 09:51:57 -0800

    1315849 - switching to the Beta version and Foreman welcome

--- lib/foreman_theme_satellite/version.rb
+++ lib/foreman_theme_satellite/version.rb
@@ -1,7 +1,7 @@
 module ForemanThemeSatellite
   VERSION = "0.1.2"
   #this file indicates the satellite version that will be represented on the login page.
-  SATELLITE_VERSION = "SATELLITE-6.2.0"
+  SATELLITE_VERSION = "Satellite 6.2.0 Beta"
   #this file indicates the satellite version that will be uesd on links to documentation.
   SATELLITE_SHORT_VERSION = "6.2"
 end
