9711007144534c803c85c6805b993932aa22d6de
Jenkins <sat6-jenkins@redhat.com>
2016-07-18 16:40:06 -0400

    Bump version to 0.1.24

--- lib/foreman_theme_satellite/version.rb
+++ lib/foreman_theme_satellite/version.rb
@@ -1,5 +1,5 @@
 module ForemanThemeSatellite
-  VERSION = "0.1.23"
+  VERSION = "0.1.24"
   #this file indicates the satellite version that will be represented on the login page.
   SATELLITE_VERSION = "Satellite 6.2.0"
   #this file indicates the satellite version that will be uesd on links to documentation.
