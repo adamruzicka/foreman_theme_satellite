f6db36c8976c9d908e07b97cd68f79ad8e856753
Eric D. Helms <ericdhelms@gmail.com>
2017-06-19 12:41:07 -0400

    Bump to 2.0.0 and set to next Satellite version

--- lib/foreman_theme_satellite/version.rb
+++ lib/foreman_theme_satellite/version.rb
@@ -1,7 +1,7 @@
 module ForemanThemeSatellite
-  VERSION = "1.0.3"
+  VERSION = "2.0.0"
   #this file indicates the satellite version that will be represented on the login page.
-  SATELLITE_VERSION = "6.3.0"
+  SATELLITE_VERSION = "6.4.0"
   #this file indicates the satellite version that will be uesd on links to documentation.
-  SATELLITE_SHORT_VERSION = "6.3"
+  SATELLITE_SHORT_VERSION = "6.4"
 end
