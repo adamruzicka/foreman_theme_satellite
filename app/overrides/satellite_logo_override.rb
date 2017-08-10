6acb46ec381615b4c56f3c451b7e9481c524e7ed
Shimon Shtein <sshtein@redhat.com>
2017-08-10 13:47:01 +0300

    BZ 1479754 - Override logout logo

--- app/overrides/satellite_logo_override.rb
+++ app/overrides/satellite_logo_override.rb
@@ -7,3 +7,8 @@ Deface::Override.new(:virtual_path  => "users/login",
                      :name          => "add redhat logo",
                      :insert_before => "div#login",
                      :text          => "<span id='badge'><%= image_tag('Redhat-logo.png') %></span>")
+
+Deface::Override.new(:virtual_path   => "users/extlogout",
+                    :name           => "satellite logo logout",
+                    :replace        => "erb[loud]:contains('login_logo.png')",
+                    :text           => '<%= image_tag("login_logo.svg") %>')
