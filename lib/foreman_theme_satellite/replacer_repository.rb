9fccf2f80101519280917615ddb9ecb1a1bad429
Shimon Shtein <sshtein@redhat.com>
2017-10-24 19:53:15 +0300

    Fix client side branding to stop using ES6 syntax

--- lib/foreman_theme_satellite/replacer_repository.rb
+++ lib/foreman_theme_satellite/replacer_repository.rb
@@ -1,42 +1,10 @@
 require 'fast_gettext'
+require_relative 'branded_words'
 
 module ForemanThemeSatellite
   # This repository is a wrapper above other repository,
   # it replaces branded words with their downstream counterparts.
   class ReplacerRepository < ::FastGettext::TranslationRepository::Base
-    FOREMAN_BRAND = {
-      /\bHTTP Proxy\b(?!-)/    => 'HTTP Proxy',
-      /\bHTTP proxy\b(?!-)/    => 'HTTP Proxy',
-      /\bIgnore Proxy\b(?!-)/  => 'Ignore Proxy',
-      /\bIgnore proxy\b(?!-)/  => 'Ignore Proxy',
-      /\bLeave this blank if no proxy is used\b(?!-)/ => 'Leave this blank if no proxy is used',
-      /\bbypass proxy settings\b(?!-)/ => 'bypass proxy settings',
-      /\bForeman\b(?!-)/       => 'Satellite',
-      /\bforeman\b(?!-)/       => 'satellite',
-      /\bsmart-proxy\b(?!-)/   => 'capsule',
-      /\bSmart-proxy\b(?!-)/   => 'Capsule',
-      /\bsmart proxies\b(?!-)/ => 'capsules',
-      /\bSmart proxies\b(?!-)/ => 'Capsules',
-      /\bsmart-proxies\b(?!-)/ => 'capsules',
-      /\bSmart-proxies\b(?!-)/ => 'Capsules',
-      /\bSmart Proxies\b(?!-)/ => 'Capsules',
-      /\bSmart Proxy\b(?!-)/   => 'Capsule',
-      /\bsmart proxy\b(?!-)/   => 'capsule',
-      /\bSmart-Proxies\b(?!-)/ => 'Capsules',
-      /\bSmart proxy\b(?!-)/   => 'Capsule',
-      /\bSmart-Proxy\b(?!-)/   => 'Capsule',
-      /\boVirt\b(?!-)/         => 'RHEV',
-      /\bovirt\b(?!-)/         => 'RHEV',
-      /\bOvirt\b(?!-)/         => 'RHEV',
-      /\bFreeIPA\b(?!-)/       => 'Red Hat Identity Management',
-      /\bOpenStack\b(?!-)/     => 'RHEL OpenStack Platform',
-      /\bopenstack\b(?!-)/     => 'RHEL OpenStack Platform',
-      /\bOpenstack\b(?!-)/     => 'RHEL OpenStack Platform',
-      /\bProxy\b(?!-)/         => 'Capsule',
-      /\bproxy\b(?!-)/         => 'Capsule',
-      /\bProxies\b(?!-)/       => 'Capsules',
-      /\bproxies\b(?!-)/       => 'Capsules' }.freeze
-
     def initialize(old_repo)
       @repo = old_repo
     end
@@ -56,7 +24,7 @@ module ForemanThemeSatellite
       val = val.dup if val
 
       replaced = nil
-      FOREMAN_BRAND.each do |foreman_word, value|
+      ForemanThemeSatellite::FOREMAN_BRAND.each do |foreman_word, value|
         replaced ||= val.gsub!(foreman_word, value) if val
       end
       replaced ? val : original
