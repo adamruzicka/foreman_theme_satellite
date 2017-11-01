48581d67f3bcb6743586aaeb80735dcdb29d8936
Eric D. Helms <ericdhelms@gmail.com>
2017-11-01 16:07:19 -0400

    BZ #1508408 - DB upgrade[6.2 -> 6.3] failed at db_migrate step: PG::Error: ERROR: update or delete on table "mail_notifications" violates foreign key constraint

--- db/migrate/20161220092005_remove_satellite_from_notification_name_spell_correction.rb
+++ db/migrate/20161220092005_remove_satellite_from_notification_name_spell_correction.rb
@@ -1,14 +1,24 @@
 class RemoveSatelliteFromNotificationNameSpellCorrection < ActiveRecord::Migration
   class FakeMailNotification < ActiveRecord::Base
     self.table_name = 'mail_notifications'
+
+    has_many :user_mail_notifications, :dependent => :destroy, :class_name => 'FakeUserMailNotification', :foreign_key => 'mail_notification_id'
+  end
+
+  class FakeUserMailNotification < ActiveRecord::Base
+    self.table_name = 'user_mail_notifications'
   end
 
   def up
-    FakeMailNotification.where(name: 'satellite_sync_errata').each do |notification|
-      new_name = 'sync_errata'
-      FakeMailNotification.where(name: new_name).destroy_all
-      notification.name = new_name
-      notification.save!
+    sync_errata_notification = FakeMailNotification.where(name: 'sync_errata').first
+    satellite_sync_errata_notification = FakeMailNotification.where(name: 'satellite_sync_errata').first
+
+    if sync_errata_notification && satellite_sync_errata_notification && FakeUserMailNotification.where(:mail_notification_id => sync_errata_notification.id).empty?
+      satellite_sync_errata_notification.user_mail_notifications.each do |user_mail_notificiation|
+        FakeUserMailNotification.create(:user_id => user_mail_notification.user_id, :mail_notification_id => sync_errata_notification.id)
+      end
     end
+
+    satellite_sync_errata_notification.destroy
   end
 end
