a0bd77d71079412feff28d4c23d586e89fac8cac
Shimon Shtein <sshtein@redhat.com>
2018-06-26 16:18:39 +0300

    BZ 1572980 - Remove provisioning templates and fix idm params

--- app/models/concerns/settings_branding.rb
+++ app/models/concerns/settings_branding.rb
@@ -6,14 +6,6 @@ module SettingsBranding
       'email_reply_address' => "satellite-noreply@#{domain}",
       'email_subject_prefix' => '[satellite]',
 
-      'katello_default_provision' => 'Satellite Kickstart Default',
-      'katello_default_finish' => 'Satellite Kickstart Default Finish',
-      'katello_default_user_data' => 'Satellite Kickstart Default User Data',
-      'katello_default_PXELinux' => 'Kickstart default PXELinux',
-      'katello_default_iPXE' => 'Kickstart default iPXE',
-      'katello_default_ptable' => 'Kickstart default',
-      'katello_default_kexec' => 'Discovery Red Hat kexec',
-      'katello_default_atomic_provision' => 'Satellite Atomic Kickstart Default',
       'rss_url' => 'https://access.redhat.com/blogs/1169563/feed'
     }.freeze
   end
