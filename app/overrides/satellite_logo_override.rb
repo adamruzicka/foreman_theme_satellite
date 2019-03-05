08e036b2e5df5f1cc3d94d3f3b62f921e1694a34
Bryan Kearney <bryan.kearney@gmail.com>
2019-03-05 14:36:48 -0500

    BZ #1687956 - Add in the new logo and background images.

--- app/overrides/satellite_logo_override.rb
+++ app/overrides/satellite_logo_override.rb
@@ -3,11 +3,6 @@ Deface::Override.new(:virtual_path   => "users/login",
                      :replace        => "erb[loud]:contains('login_logo.png')",
                      :text           => '<%= image_tag("login_logo.svg") %>')
 
-Deface::Override.new(:virtual_path     => "users/login",
-                     :name             => "add redhat logo",
-                     :replace_contents => "span#badge",
-                     :text             => "<%= image_tag('Redhat-logo.svg') %>")
-
 Deface::Override.new(:virtual_path   => "users/extlogout",
                      :name           => "satellite logo logout",
                      :replace        => "erb[loud]:contains('login_logo.png')",
