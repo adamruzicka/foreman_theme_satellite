1bc17047a4d08e35f2e7e45251f363eacf787a8d
Dominik Matoulek <domitea@gmail.com>
2021-09-01 14:29:54 +0200

    BZ #1996490 - Fixing bad definition of enabled bootdisk types

--- app/models/concerns/settings_branding.rb
+++ app/models/concerns/settings_branding.rb
@@ -9,7 +9,7 @@ module SettingsBranding
 
         'rss_url' => 'https://www.redhat.com/en/rss/blog/channel/red-hat-satellite',
         'foreman_tasks_troubleshooting_url' => 'https://access.redhat.com/solutions/satellite6-tasks#%{label}',
-        'bootdisk_allowed_types' => '[subnet,full_host]'
+        'bootdisk_allowed_types' => ['subnet','full_host']
       }.freeze
     end
 
