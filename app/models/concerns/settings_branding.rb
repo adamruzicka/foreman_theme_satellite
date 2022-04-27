2b48b3165c2e4ed744afdeb63b10d1780ba775ff
Shimon Shtein <sshtein@redhat.com>
2022-04-27 13:59:50 +0300

    BZ 2073421 Set default to old host UI

--- app/models/concerns/settings_branding.rb
+++ app/models/concerns/settings_branding.rb
@@ -3,6 +3,7 @@ module SettingsBranding
   def branded_settings
     domain = SETTINGS[:domain]
     @branded_settings ||= {
+      'host_details_ui' => false,
       'email_reply_address' => "satellite-noreply@#{domain}",
       'email_subject_prefix' => '[satellite]',
 
@@ -13,7 +14,7 @@ module SettingsBranding
   end
 
   def _add(name, default:, **rest)
-    branded_default = branded_settings[name.to_s]
-    super(name, default: branded_default || default, **rest)
+    branded_default = branded_settings.fetch(name.to_s, default)
+    super(name, default: branded_default, **rest)
   end
 end
