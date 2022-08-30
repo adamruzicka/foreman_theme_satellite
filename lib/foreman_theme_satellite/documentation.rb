7f70e0746416d6e61ee6a2cea79d3f4f41e597bf
Eric D. Helms <ericdhelms@gmail.com>
2022-08-30 15:07:03 +0000

    Fix documentation links and tests

--- lib/foreman_theme_satellite/documentation.rb
+++ lib/foreman_theme_satellite/documentation.rb
@@ -4,57 +4,69 @@
 module ForemanThemeSatellite
   module Documentation
     USER_GUIDE_DICTIONARY = {
-      'using-config-groups' => "#{ForemanThemeSatellite.documentation_root}/puppet_guide/chap-red_hat_satellite-puppet_guide-using_config_groups_to_manage_puppet_classes",
-      'SmartMatchers' => "#{ForemanThemeSatellite.documentation_root}/puppet_guide/chap-red_hat_satellite-puppet_guide-adding_puppet_modules_to_red_hat_satellite_6#sect-Red_Hat_Satellite-Puppet_Guide-Adding_Puppet_Modules_to_Red_Hat_Satellite_6-Configuring_Smart_Class_Parameters",
-      'ImportingRoles' => "#{ForemanThemeSatellite.documentation_root}/configuring_satellite_to_use_ansible/getting-started-with-ansible_ansible#importing-Ansible-variables_ansible",
-      'Variables' => "#{ForemanThemeSatellite.documentation_root}/configuring_satellite_to_use_ansible/getting-started-with-ansible_ansible#creating-Ansible-variables_ansible",
-      'ForemanRemoteExecution' => "#{ForemanThemeSatellite.documentation_root}/managing_hosts/configuring-and-setting-up-remote-jobs_managing-hosts#about-running-jobs-on-hosts_managing-hosts",
-      'ExecutingaJob' => "#{ForemanThemeSatellite.documentation_root}/managing_hosts/configuring-and-setting-up-remote-jobs_managing-hosts#configuring-the-global-smartproxy-remote-execution-setting_managing-hosts",
-      'JobTemplates' => "#{ForemanThemeSatellite.documentation_root}/managing_hosts/configuring-and-setting-up-remote-jobs_managing-hosts#setting-up-job-templates_managing-hosts",
-      'FactsandtheENC' => "#{ForemanThemeSatellite.documentation_root}/puppet_guide/chap-red_hat_satellite-puppet_guide-storing_and_maintaining_host_information#sect-Red_Hat_Satellite-Puppet_Guide-Storing_and_Maintaining_Host_Information-Using_Facter_and_Facts",
-      'Architectures' => "#{ForemanThemeSatellite.documentation_root}/provisioning_guide/configuring-provisioning-resources#configuring-provisioning-resources-Architectures",
-      'Provisioning' => "#{ForemanThemeSatellite.documentation_root}/provisioning_guide/",
-      'PuppetReports' => "#{ForemanThemeSatellite.documentation_root}/administering_red_hat_satellite/chap-red_hat_satellite-administering_red_hat_satellite-monitoring_resources#chap-Red_Hat_Satellite-Administering_Red_Hat_Satellite-Using_the_Red_Hat_Satellite_Content_Dashboard",
-      'Realm' => "#{ForemanThemeSatellite.documentation_root}/administering_red_hat_satellite/chap-red_hat_satellite-administering_red_hat_satellite-configuring_external_authentication#sect-Red_Hat_Satellite-Administering_Red_Hat_Satellite-Configuring_External_Authentication-External_Authentication_for_Provisioned_Hosts",
-      'Searching' => "#{ForemanThemeSatellite.documentation_root}/administering_red_hat_satellite/chap-red_hat_satellite-administering_red_hat_satellite-searching_and_bookmarking",
-      'InstallationMedia' => "#{ForemanThemeSatellite.documentation_root}/provisioning_guide/configuring-provisioning-resources#configuring-provisioning-resources-Creating_Installation_Media",
-      'ParameterizedClasses' => "#{ForemanThemeSatellite.documentation_root}/puppet_guide/chap-red_hat_satellite-puppet_guide-adding_puppet_modules_to_red_hat_satellite_6#sect-Red_Hat_Satellite-Puppet_Guide-Adding_Puppet_Modules_to_Red_Hat_Satellite_6-Importing_Parameterized_Classes_from_a_Puppet_Master",
-      'PartitionTables' => "#{ForemanThemeSatellite.documentation_root}/provisioning_guide/configuring-provisioning-resources#configuring-provisioning-resources-Creating_Partition_Tables",
-      'ProvisioningTemplates' => "#{ForemanThemeSatellite.documentation_root}/provisioning_guide/configuring-provisioning-resources#creating-provisioning-templates_provisioning",
-      'OperatingSystems' => "#{ForemanThemeSatellite.documentation_root}/provisioning_guide/configuring-provisioning-resources#configuring-provisioning-resources-Creating_Operating_Systems",
-      'RolesandPermissions' => "#{ForemanThemeSatellite.documentation_root}/administering_red_hat_satellite/chap-red_hat_satellite-administering_red_hat_satellite-users_and_roles",
-      'Parameters' => "#{ForemanThemeSatellite.documentation_root}/puppet_guide/chap-red_hat_satellite-puppet_guide-adding_puppet_modules_to_red_hat_satellite_6#sect-Red_Hat_Satellite-Puppet_Guide-Adding_Puppet_Modules_to_Red_Hat_Satellite_6-Parameters",
-      'LDAPAuthentication' => "#{ForemanThemeSatellite.documentation_root}/administering_red_hat_satellite/chap-red_hat_satellite-administering_red_hat_satellite-configuring_external_authentication",
-      'VMwareNotes' => "#{ForemanThemeSatellite.documentation_root}/provisioning_guide/provisioning_virtual_machines_in_vmware_vsphere",
-      'RackspaceNotes' => "#{ForemanThemeSatellite.documentation_root}/provisioning_guide",
-      'RHEVNotes' => "#{ForemanThemeSatellite.documentation_root}/provisioning_guide/provisioning-virtual-machines-rhv_provisioning",
-      'OpenStackNotes' => "#{ForemanThemeSatellite.documentation_root}/provisioning_guide/provisioning-cloud-instances-openstack",
-      'LibvirtNote' => "#{ForemanThemeSatellite.documentation_root}/provisioning_guide/provisioning-virtual-machines-rhv_provisioning",
-      'EC2' => "#{ForemanThemeSatellite.documentation_root}/provisioning_guide/provisioning_cloud_instances_in_amazon_ec2",
-      'GoogleComputeEngineNotes' => "#{ForemanThemeSatellite.documentation_root}/provisioning_guide/provisioning-cloud-instances-gce",
-      'ComputeResources' => "#{ForemanThemeSatellite.documentation_root}/provisioning_guide/provisioning-introduction#provisioning-overview_provisioning",
-      'SmartProxies' => "#{ForemanThemeSatellite.documentation_root}/planning_for_red_hat_satellite/chap-documentation-architecture_guide-capsule_server_overview",
-      'SmartVariables' => "#{ForemanThemeSatellite.documentation_root}/puppet_guide/chap-red_hat_satellite-puppet_guide-adding_puppet_modules_to_red_hat_satellite_6#sect-Red_Hat_Satellite-Puppet_Guide-Adding_Puppet_Modules_to_Red_Hat_Satellite_6-Configuring_Smart_Variables",
+      # Ansible
+      'ImportingRoles' => "#{ForemanThemeSatellite.documentation_root}/configuring_red_hat_satellite_to_use_ansible/getting-started-with-ansible_ansible#importing-Ansible-roles_ansible",
+      'Variables' => "#{ForemanThemeSatellite.documentation_root}/configuring_red_hat_satellite_to_use_ansible/getting-started-with-ansible_ansible#overriding-Ansible-variables_ansible",
+
+      # Managing Hosts
+      'ForemanRemoteExecution' => "#{ForemanThemeSatellite.documentation_root}/managing_hosts/configuring_and_setting_up_remote_jobs_managing-hosts",
+      'ExecutingaJob' => "#{ForemanThemeSatellite.documentation_root}/managing_hosts/configuring_and_setting_up_remote_jobs_managing-hosts#configuring-the-global-smartproxy-remote-execution-setting_managing-hosts",
+      'JobTemplates' => "#{ForemanThemeSatellite.documentation_root}/managing_hosts/configuring_and_setting_up_remote_jobs_managing-hosts#setting-up-job-templates_managing-hosts",
+      'ReportTemplates' => "#{ForemanThemeSatellite.documentation_root}/managing_hosts/using_report_templates_to_monitor_hosts_managing-hosts",
+      'Reports' => "#{ForemanThemeSatellite.documentation_root}/managing_hosts/using_report_templates_to_monitor_hosts_managing-hosts",
+
+      # Provisioning Hosts
+      'Architectures' => "#{ForemanThemeSatellite.documentation_root}/provisioning_hosts/configuring_provisioning_resources_provisioning#creating-architectures_provisioning",
+      'Provisioning' => "#{ForemanThemeSatellite.documentation_root}/provisioning_hosts/",
+      'InstallationMedia' => "#{ForemanThemeSatellite.documentation_root}/provisioning_hosts/configuring_provisioning_resources_provisioning#adding-installation-media_provisioning",
+      'PartitionTables' => "#{ForemanThemeSatellite.documentation_root}/provisioning_hosts/configuring_provisioning_resources_provisioning#creating-partition-tables_provisioning",
+      'ProvisioningTemplates' => "#{ForemanThemeSatellite.documentation_root}/provisioning_hosts/configuring_provisioning_resources_provisioning#provisioning-templates_provisioning",
+      'OperatingSystems' => "#{ForemanThemeSatellite.documentation_root}/provisioning_hosts/configuring_provisioning_resources_provisioning#creating-operating-systems_provisioning",
+      'UsingComputeProfiles' => "#{ForemanThemeSatellite.documentation_root}/provisioning_hosts/configuring_provisioning_resources_provisioning#creating-compute-profiles_provisioning",
+      'ComputeResources' => "#{ForemanThemeSatellite.documentation_root}/provisioning_hosts/",
+      'NoVNC' => "#{ForemanThemeSatellite.documentation_root}/provisioning_hosts/configuring_provisioning_resources_provisioning#using-novnc-to-access-virtual-machines_provisioning",
+      'VMwareNotes' => "#{ForemanThemeSatellite.documentation_root}/provisioning_hosts/provisioning_virtual_machines_in_vmware_provisioning",
+      'RHEVNotes' => "#{ForemanThemeSatellite.documentation_root}/provisioning_hosts/provisioning_virtual_machines_on_ovirt_provisioning",
+      'OpenStackNotes' => "#{ForemanThemeSatellite.documentation_root}/provisioning_hosts/provisioning_cloud_instances_openstack_provisioning",
+      'LibvirtNote' => "#{ForemanThemeSatellite.documentation_root}/provisioning_hosts/provisioning_virtual_machines_on_kvm_provisioning",
+      'EC2' => "#{ForemanThemeSatellite.documentation_root}/provisioning_hosts/provisioning_cloud_instances_in_amazon_ec2_provisioning",
+      'GoogleComputeEngineNotes' => "#{ForemanThemeSatellite.documentation_root}/provisioning_hosts/provisioning_cloud_instances_on_google_compute_engine_provisioning",
+      'Networking' => "#{ForemanThemeSatellite.documentation_root}/provisioning_hosts/configuring_networking_provisioning#Adding_a_Subnet_to_Server_provisioning",
+
+      # Puppet Guide
+      'FactsandtheENC' => "#{ForemanThemeSatellite.documentation_root}/managing_configurations_using_puppet_integration_in_red_hat_satellite/introducing-configuration-management-using-puppet_managing-configurations-puppet#performing-configuration-management_managing-configurations-puppet",
+      'using-config-groups' => "#{ForemanThemeSatellite.documentation_root}/managing_configurations_using_puppet_integration_in_red_hat_satellite/creating-a-puppet-config-group_managing-configurations-puppet",
+      'SmartMatchers' => "#{ForemanThemeSatellite.documentation_root}/managing_configurations_using_puppet_integration_in_red_hat_satellite/configuring_puppet_smart_class_parameters_managing-configurations-puppet",
+      'ParameterizedClasses' => "#{ForemanThemeSatellite.documentation_root}/managing_configurations_using_puppet_integration_in_red_hat_satellite/importing_puppet_classes_and_environments_managing-configurations-puppet#doc-wrapper",
+      'Parameters' => "#{ForemanThemeSatellite.documentation_root}/managing_configurations_using_puppet_integration_in_red_hat_satellite/configuring_puppet_smart_class_parameters_managing-configurations-puppet",
+      'Classes' => "#{ForemanThemeSatellite.documentation_root}/managing_configurations_using_puppet_integration_in_red_hat_satellite/importing_puppet_classes_and_environments_managing-configurations-puppet",
+      'Environments' => "#{ForemanThemeSatellite.documentation_root}/managing_configurations_using_puppet_integration_in_red_hat_satellite/creating_a_custom_puppet_environment_managing-configurations-puppet",
+
+      # Administering
       'ConfigurationOptions' => "#{ForemanThemeSatellite.documentation_root}/administering_red_hat_satellite/",
-      'Trends' => "#{ForemanThemeSatellite.documentation_root}/administering_red_hat_satellite/chap-red_hat_satellite-administering_red_hat_satellite-monitoring_resources#monitoring_trends",
-      'Classes' => "#{ForemanThemeSatellite.documentation_root}/puppet_guide/chap-red_hat_satellite-puppet_guide-adding_puppet_modules_to_red_hat_satellite_6#sect-Red_Hat_Satellite-Puppet_Guide-Adding_Puppet_Modules_to_Red_Hat_Satellite_6-Importing_Parameterized_Classes_from_a_Puppet_Master",
-      'Environments' => "#{ForemanThemeSatellite.documentation_root}/puppet_guide/chap-red_hat_satellite-puppet_guide-adding_puppet_modules_to_red_hat_satellite_6#sect-Red_Hat_Satellite-Puppet_Guide-Puppet_Environments",
-      'Bookmarks' => "#{ForemanThemeSatellite.documentation_root}/administering_red_hat_satellite/chap-red_hat_satellite-administering_red_hat_satellite-searching_and_bookmarking#sect-Red_Hat_Satellite-Administering_Red_Hat_Satellite-Searching_and_Bookmarking-Bookmarking",
-      'NoVNC' => "#{ForemanThemeSatellite.documentation_root}/provisioning_guide/configuring-provisioning-resources#configuring-provisioning-resources-Accessing_Virtual_Machines_with_the_noVNC_Console",
-      'HTTP(S)Proxy' => "#{ForemanThemeSatellite.documentation_root}/installing_satellite_server_from_a_connected_network/performing-additional-configuration#configuring-satellite-with-an-http-proxy_satellite",
+      'PuppetReports' => "#{ForemanThemeSatellite.documentation_root}/administering_red_hat_satellite/monitoring_resources_admin#Using_the_Project_Content_Dashboard_admin",
+      'Realm' => "#{ForemanThemeSatellite.documentation_root}/administering_red_hat_satellite/configuring_external_authentication_admin#External_Authentication_for_Provisioned_Hosts_admin",
+      'Searching' => "#{ForemanThemeSatellite.documentation_root}/administering_red_hat_satellite/searching_and_bookmarking_admin",
+      'RolesandPermissions' => "#{ForemanThemeSatellite.documentation_root}/administering_red_hat_satellite/managing_users_and_roles_admin",
+      'LDAPAuthentication' => "#{ForemanThemeSatellite.documentation_root}/administering_red_hat_satellite/configuring_external_authentication_admin#Using_LDAP_admin",
+      'Bookmarks' => "#{ForemanThemeSatellite.documentation_root}/administering_red_hat_satellite/searching_and_bookmarking_admin",
+      'Policydeploymentoptions' => "#{ForemanThemeSatellite.documentation_root}/administering_red_hat_satellite/managing_security_compliance_admin#Managing_Compliance_Policies_admin",
+      'CreatingSCAPcontent' => "#{ForemanThemeSatellite.documentation_root}/administering_red_hat_satellite/running_openscap_scans_admin",
+
+      # Planning
+      'SmartProxies' => "#{ForemanThemeSatellite.documentation_root}/satellite_overview_concepts_and_deployment_considerations/chap-documentation-architecture_guide-capsule_server_overview",
+      'Auditing' => "#{ForemanThemeSatellite.documentation_root}/satellite_overview_concepts_and_deployment_considerations/appe-architecture_guide-glossary_of_terms#varl-Glossary_of_Terms-Audits",
+
+      # Install
+      'HTTP(S)Proxy' => "#{ForemanThemeSatellite.documentation_root}//installing_satellite_server_in_a_connected_network_environment/performing-additional-configuration#Configuring_Server_with_an_HTTP_Proxy_satellite",
+
+      # Misc
       'GettingHelp' => "https://access.redhat.com/products/red-hat-satellite/#support",
-      'Policydeploymentoptions' => "#{ForemanThemeSatellite.documentation_root}/administering_red_hat_satellite/chap-red_hat_satellite-administering_red_hat_satellite-security_compliance_management#creating-a-complicance-policy_assembly",
-      'Auditing' => "#{ForemanThemeSatellite.documentation_root}/planning_for_red_hat_satellite/appe-red_hat_satellite-architecture_guide-glossary_of_terms#varl-Glossary_of_Terms-Audits",
-      'ReportTemplates' => "#{ForemanThemeSatellite.documentation_root}/managing_hosts/using_report_templates",
-      'UsingComputeProfiles' => "#{ForemanThemeSatellite.documentation_root}/provisioning_guide/configuring-provisioning-resources#creating-compute-profiles_provisioning",
-      'Networking' => "#{ForemanThemeSatellite.documentation_root}/provisioning_guide/configuring_networking#Configuring_Networking-Adding_a_Subnet_to_the_Satellite_Server",
-      'Reports' => "#{ForemanThemeSatellite.documentation_root}/managing_hosts/using_report_templates",
-      'CreatingSCAPcontent' => "#{ForemanThemeSatellite.documentation_root}/administering_red_hat_satellite/chap-administering-security_compliance_management#sect-Administering-Security_Compliance_Management_with_OpenSCAP-Configuring_SCAP_Content",
     }.freeze
 
     PLUGINS_DOCUMENTATION = {
-      'foreman_azure' => "#{ForemanThemeSatellite.documentation_root}/provisioning_guide/provisioning-cloud-instances-azure",
-      'foreman_discovery' => "#{ForemanThemeSatellite.documentation_root}/provisioning_guide/configuring-the-discovery-service",
+      'foreman_azure' => "#{ForemanThemeSatellite.documentation_root}/provisioning_hosts/provisioning_cloud_instances_on_microsoft_azure_resource_manager_provisioning",
+      'foreman_discovery' => "#{ForemanThemeSatellite.documentation_root}/provisioning_hosts/configuring_the_discovery_service_provisioning",
     }.freeze
 
     SPECIAL_LINKS = [
--- test/unit/settings_branding_test.rb
+++ test/unit/settings_branding_test.rb
@@ -2,23 +2,12 @@ require 'test_plugin_helper'
 
 class SettingsBrandingTest < ActiveSupport::TestCase
   describe 'stubbed creation' do
-    teardown do
-      Setting.unstub(:create_existing)
-    end
-
-    test 'it replaces value to a branded one using models' do
-      Setting.expects(:create_existing).with{ |s, opts|
-        opts[:default] == Setting.branded_settings['email_subject_prefix']
-      }
-      props = Setting.set('email_subject_prefix', N_("Prefix to add to all outgoing email"), '[foreman]', N_('Email subject prefix'))
-      Setting.create! props.update(:category => "Setting")
-    end
 
     test 'it replaces value to a branded one using DSL' do
       Setting.where(name: 'dsl_setting').delete_all
-      branded_settings = Setting.branded_settings.deep_dup
+      branded_settings = Foreman.settings.branded_settings.deep_dup
       branded_settings['dsl_setting'] = 'TEST_VALUE'
-      Setting.stubs(:branded_settings).returns(branded_settings)
+      Foreman.settings.stubs(:branded_settings).returns(branded_settings)
 
       Foreman::SettingManager.define('TEST_ID') do
         category(:General) do
@@ -37,5 +26,6 @@ class SettingsBrandingTest < ActiveSupport::TestCase
       # Assert that the value is properly branded despite the definition.
       assert_equal 'TEST_VALUE', Setting['dsl_setting']
     end
+
   end
 end
