eff45c10068bbcb3ff3eab75809829970fe7e47d
Chris Roberts <chrobert@redhat.com>
2023-02-13 20:37:10 +0000

    BZ #2168330 - Change distributor version to match satellite version

--- app/models/concerns/distributor_version.rb
+++ app/models/concerns/distributor_version.rb
@@ -2,6 +2,6 @@ module DistributorVersion
   extend ActiveSupport::Concern
 
   included do
-    const_set(:DISTRIBUTOR_VERSION, "sat-#{Foreman::Version.new(ForemanThemeSatellite::SATELLITE_VERSION).major}".freeze)
+    const_set(:DISTRIBUTOR_VERSION, "sat-#{ForemanThemeSatellite::SATELLITE_SHORT_VERSION}".freeze)
   end
 end
